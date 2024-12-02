e0732639df808890df55201b0ee684f9570b1e3b
Daniel Seidel <daniel@RN-145-90-174-196.eduroam.rug.nl>
2024-12-02 13:53:49 +0100

    Little simplification

--- split_dataset.py
+++ split_dataset.py
@@ -117,52 +117,18 @@ def main():
     # save the token sequences to .npy files to be used in model generation
     np.save(os.path.join(npy_arrays_base, SPLIT + "_dataset.npy"), prompts)
     # split the tokens into preprefix, prefix, and suffix
-
-    if EXAMPLE_TOKEN_LEN == 250:
+    if EXAMPLE_TOKEN_LEN > 100:
         np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_preprefix.npy"), prompts[:, :150]
+            os.path.join(npy_arrays_base, SPLIT + "_preprefix.npy"), prompts[:, :(EXAMPLE_TOKEN_LEN-100)]
         )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_prefix.npy"), prompts[:, 150:200]
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_suffix.npy"), prompts[:, 200:250]
-        )
-
-    if EXAMPLE_TOKEN_LEN == 200:
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_preprefix.npy"), prompts[:, :100]
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_prefix.npy"), prompts[:, 100:150]
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_suffix.npy"), prompts[:, 150:200]
-        )
-
-    if EXAMPLE_TOKEN_LEN == 150:
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_preprefix.npy"), prompts[:, :50]
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_prefix.npy"), prompts[:, 50:100]
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_suffix.npy"), prompts[:, 100:150]
-        )
-
-    elif EXAMPLE_TOKEN_LEN == 100:
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_prefix.npy"),
-            prompts[:, 0:PREFIX_LEN],
-        )
-        np.save(
-            os.path.join(npy_arrays_base, SPLIT + "_suffix.npy"),
-            prompts[:, PREFIX_LEN : PREFIX_LEN + SUFFIX_LEN],
-        )
+    np.save(
+        os.path.join(npy_arrays_base, SPLIT + "_prefix.npy"), prompts[:, (EXAMPLE_TOKEN_LEN-100):(EXAMPLE_TOKEN_LEN-50)]
+    )
+    np.save(
+        os.path.join(npy_arrays_base, SPLIT + "_suffix.npy"), prompts[:, (EXAMPLE_TOKEN_LEN - 50):EXAMPLE_TOKEN_LEN]
+    )
 
     logger.info("===== Done ======")
-
 
 if __name__ == "__main__":
     main()
