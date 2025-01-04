1c6637f2f01aa1f56b72107bd3b0de87358ccd85
danielSe04 <dan.seidel.2004@gmx.de>
2025-01-04 18:05:44 +0100

    This belongs to the last commit

--- habrok-scripts/EMEA/100/temp-nl.sh
+++ habrok-scripts/EMEA/100/temp-nl.sh
@@ -13,6 +13,7 @@ module load Boost/1.79.0-GCC-11.3.0
 
 source $HOME/thesis-llm-privacy/.env/bin/activate
 
+python ./trainer.py --config_file exp-configs/EMEA/100/temp-nl.json
 python ./mia.py --config_file exp-configs/EMEA/100/temp-nl.json --model_dir /scratch/s5202841/finetuned/EMEA/nl-100-nat-125M-nb
 
 deactivate
