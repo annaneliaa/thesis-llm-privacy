a13a53b4533b6f7e65656ba2a08fff87cbc1cc3e
danielSe04 <dan.seidel.2004@gmx.de>
2025-01-09 20:34:55 +0100

    Adapted time for experiments

--- habrok-scripts/EMEA/100/experiment-en-1.3B.sh
+++ habrok-scripts/EMEA/100/experiment-en-1.3B.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=06:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=experiment-full(100/EN/1.3B)
--- habrok-scripts/EMEA/100/experiment-en-2.7B.sh
+++ habrok-scripts/EMEA/100/experiment-en-2.7B.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=09:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=experiment-full(100/EN/2.7B)
